a194c03ae72a99afa8758690ab2a46351177de7f
KyoCheol <kok1434@naver.com>
2021-10-29 13:23:11 +0900

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="app">
+    <!-- 추가된 헤더 컴포넌트 사용 -->
+    <Header />
+
+    <Content />
+    <!-- 추가된 풋터 컴포넌트 사용 -->
+    <Footer />
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import Content from "@/components/HelloWorld";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+    Content,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
