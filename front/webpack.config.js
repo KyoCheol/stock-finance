3321b7dee6a3c691b71ad596bf4b4064b11789e0
KyoCheol <kok1434@naver.com>
2021-10-29 13:19:58 +0900

    Update webpack.config.js

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,9 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src/')
-        }
-    }
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  rules: [
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        "vue-style-loader",
+        "css-loader",
+        {
+          loader: "sass-loader",
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require("sass"),
+            fiber: require("fibers"),
+            indentedSyntax: true, // optional
+          },
+        },
+      ],
+    },
+  ],
 };
