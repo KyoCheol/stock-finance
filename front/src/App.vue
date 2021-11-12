1d50333f5d54fa189111a0ff979b6b238218bb28
KyoCheol <kok1434@naver.com>
2021-11-12 15:21:24 +0900

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <v-app id="app">
-    <!-- 추가된 헤더 컴포넌트 사용 -->
-    <Header />
+    <router-view />
+    <!--    &lt;!&ndash; 추가된 헤더 컴포넌트 사용 &ndash;&gt;-->
+    <!--    <Header />-->
 
-    <Content />
-    <!-- 추가된 풋터 컴포넌트 사용 -->
-    <Footer />
+    <!--    <Content />-->
+    <!--    &lt;!&ndash; 추가된 풋터 컴포넌트 사용 &ndash;&gt;-->
+    <!--    <Footer />-->
   </v-app>
 </template>
 
 <script>
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import Content from "@/components/HelloWorld";
+// import Header from "@/components/layout/Header";
+// import Footer from "@/components/layout/Footer";
+// import Content from "@/components/HelloWorld";
 
 export default {
   name: "App",
   components: {
-    Header,
-    Footer,
-    Content,
+    // Header,
+    // Footer,
+    // Content,
   },
   data: () => ({
     //
