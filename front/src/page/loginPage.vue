d5fcc084f21aa832b82a80053f56860e18bd4330
KyoCheol <kok1434@naver.com>
2021-11-12 15:11:54 +0900

    Create loginPage.vue

---
+++ front/src/page/loginPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    prepend-icon="person"
+                    name="login"
+                    label="Login"
+                    type="text"
+                    v-model="id"
+                  ></v-text-field>
+                  <v-text-field
+                    id="password"
+                    prepend-icon="lock"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" @click="register">Register</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="login()">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      id: "",
+      password: "",
+    };
+  },
+  props: {
+    source: String,
+  },
+  methods: {
+    login() {
+      if (this.id && this.password) {
+        let id = this.id;
+        let password = this.password;
+
+        this.$store.dispatch("login", { id, password });
+      } else {
+        alert("test!");
+        return false;
+      }
+    },
+    register() {
+      this.$router.push("/register");
+    },
+  },
+};
+</script>
