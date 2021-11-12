3fc6a02a94b032511db7837adaf76065cbfa91b1
KyoCheol <kok1434@naver.com>
2021-11-12 15:21:03 +0900

    Update main.js

--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/router";
+import { store } from "./store";
+import "vuetify/dist/vuetify.min.css";
 
 Vue.config.productionTip = false;
+
+import { getAxios } from "./config/axios/axiosConfig";
+Vue.prototype.$http = getAxios;
+
+//Vue.use(vuetify, {
+//  theme: {
+//    primary: "#7957d5",
+//  },
+//});
 
 new Vue({
   router,
   store,
+//  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
