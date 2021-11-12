588c1520cae70e2bebd181074a3968f91b996aca
KyoCheol <kok1434@naver.com>
2021-11-12 15:18:18 +0900

    Create Header.vue

---
+++ front/src/components/layout/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card>
+    <v-toolbar color="cyan" dark flat>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Your Dashboard</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        color="primary"
+        outlined
+        @click="goToLoginPage"
+        v-if="!this.$store.getters.loggedIn"
+      >
+        Sign in
+      </v-btn>
+
+      <v-btn
+        color="primary"
+        outlined
+        @click="logoutBtn"
+        v-else-if="this.$store.getters.loggedIn"
+      >
+        Logout
+      </v-btn>
+
+      <!--      <v-btn icon>-->
+      <!--        <v-icon>mdi-dots-vertical</v-icon>-->
+      <!--      </v-btn>-->
+
+      <template v-slot:extension>
+        <v-tabs v-model="tab" align-with-title>
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+
+          <v-tab v-for="item in items" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <!--    <v-tabs-items v-model="tab">-->
+    <!--      <v-tab-item v-for="item in items" :key="item">-->
+    <!--        <v-card flat>-->
+    <!--          <v-card-text v-text="text"></v-card-text>-->
+    <!--        </v-card>-->
+    <!--      </v-tab-item>-->
+    <!--    </v-tabs-items>-->
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: null,
+      items: ["web", "shopping", "videos", "images", "news"],
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    };
+  },
+  methods: {
+    goToLoginPage() {
+      this.$router.push("/login");
+    },
+    logoutBtn() {
+      this.$store.dispatch("logout");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
