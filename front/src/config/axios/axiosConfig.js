2a6d5dad5e19390c5d808d833ece8170dac033ef
KyoCheol <kok1434@naver.com>
2021-11-12 15:12:22 +0900

    Create axiosConfig.js

---
+++ front/src/config/axios/axiosConfig.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export { getAxios };
+
+let getAxios = createInstance();
+
+function createInstance() {
+  const getAxios = axios.create();
+  getAxios.defaults.baseURL = "http://localhost:8080";
+  // getAxios.defaults.withCredentials = true;
+  // getAxios.defaults.crossDomain = true;
+  getAxios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+  getAxios.defaults.headers["Access-Control-Allow-Methods"] =
+    "GET,PUT,POST,DELETE,OPTIONS";
+  getAxios.defaults.headers["Access-Control-Allow-headers"] =
+    "Content-Type, Authorization, Content-Length, X-Requested-With";
+  getAxios.defaults.headers["Content-Type"] = "application/json";
+  return getAxios;
+}
