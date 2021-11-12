ebf1c3d3ca72d5fa2c3b4973b09026723403ab0a
KyoCheol <kok1434@naver.com>
2021-11-12 15:07:40 +0900

    Create News.vue

---
+++ front/src/views/News.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container> </v-container>
+</template>
+
+<script>
+export default {
+  name: "news",
+  components: {},
+  data: () => ({
+    url: {
+      newsList: "/api/v1/finance/stock/getNewsList",
+    },
+  }),
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.getNewsList();
+    },
+    getNewsList() {
+      // let url = this.url.newsList;
+      // this.$http
+      //   .get(url)
+      //   .then((res) => {
+      //     console.log(res.data);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err.data);
+      //   });
+    },
+  },
+};
+</script>
