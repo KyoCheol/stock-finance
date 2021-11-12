5870ff0eb2f1c5e5546be80f4410e76a040fbcd8
KyoCheol <kok1434@naver.com>
2021-11-12 15:44:12 +0900

    Delete src/main/java/com/board/My/Board/repository directory

--- src/main/java/com/board/My/Board/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.board.My.Board.repository;
-
-import com.board.My.Board.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
