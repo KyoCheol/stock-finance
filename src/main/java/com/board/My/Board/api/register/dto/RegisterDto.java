f24e5c68b32a8a9ccb61beaebf42bd27ec85ccfb
KyoCheol <kok1434@naver.com>
2021-11-12 15:37:14 +0900

    Create RegisterDto.java

---
+++ src/main/java/com/board/My/Board/api/register/dto/RegisterDto.java
@@ -0,0 +1,54 @@
+package com.board.seochu.finance.api.register.dto;
+
+import com.board.seochu.finance.api.member.domain.entity.Members;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class RegisterDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "'email' is a required input value")
+    @Email(message = "do not email type")
+    private String email;
+
+    @NotBlank(message = "'password' is a required input value")
+    private String password;
+
+    @NotBlank(message = "'name' is a required input value")
+    private String name;
+
+    @NotBlank(message = " 'role' selection is required ")
+    private String role;
+
+    @NotBlank(message = "'nickname' is a required input value")
+    private String nickname;
+
+    @NotBlank(message = "'mobile' is a required input value")
+    private String mobile;
+
+    public Members toEntity() {
+        Members build = Members.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .name(name)
+                .nickname(nickname)
+                .mobile(mobile)
+                .role(role)
+                .build();
+        return build;
+    }
+}
