14b13c7c518883cd592a67d3643bf2018e48bdac
KyoCheol <kok1434@naver.com>
2021-11-12 15:38:21 +0900

    Create StcConst.java

---
+++ src/main/java/com/board/My/Board/common/consts/StcConst.java
@@ -0,0 +1,17 @@
+package com.board.seochu.finance.common.consts;
+
+public class StcConst {
+    private StcConst() {
+        super();
+    }
+
+    public static final String REST_URL = "/api/vi";
+
+    public static final String CLIENT_ID = "rr8Jf4VM6dtX2vEtwZMM";
+    public static final String CLIENT_SECRET = "ZBM5GWMu1d";
+
+    public static final String BEARER_TYPE = "bearer ";
+    public static final String AUTHORITIES_KEY = "auth";
+    public static final String ACCESS_USER_ID = "id";
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 12;
+}
