3ca527185c1d5bef759f6de72c2bd5573d30369c
KyoCheol <kok1434@naver.com>
2021-11-12 15:47:05 +0900

    Create CustomUserDetailService.java

---
+++ src/main/java/com/board/My/Board/util/auth/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package com.board.seochu.finance.util.auth;
+
+import com.board.seochu.finance.api.member.domain.entity.Members;
+import com.board.seochu.finance.api.member.domain.repository.MemberRepository;
+import com.board.seochu.finance.exception.custom.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Members entity = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("존재하지 않는 회원의 정보입니다."));
+
+        UserDetails member = new CustomUserDetails(entity.getId(), email, "USER");
+
+        return member;
+    }
+}
