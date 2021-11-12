01ea3e02fb05ca8bf42ae6dee98ca4358875f510
KyoCheol <kok1434@naver.com>
2021-11-12 15:08:19 +0900

    Update index.js

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,11 +1,22 @@
+// import store from "./legoStore";
+//
+// export default store;
+
 import Vue from "vue";
 import Vuex from "vuex";
+import modules from "./modules/modules";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+const persistedState = createPersistedState({
+  paths: ["token", "id", "name", "role", "nickname"],
+});
+
+export const store = new Vuex.Store({
+  state: modules.state,
+  getters: modules.getters,
+  mutations: modules.mutations,
+  actions: modules.actions,
+  plugins: [persistedState],
 });
