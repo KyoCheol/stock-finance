30b5cd0ee9a4f9383c44bf67c93e5a27a8b49922
KyoCheol <kok1434@naver.com>
2021-11-18 16:48:50 +0900

    Create UserDetailsServiceImpl.java

---
+++ src/main/java/com/board/My/Board/api/user/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.board.seochu.finance.api.user.service;
+
+import com.board.seochu.finance.api.user.domain.entity.User;
+import com.board.seochu.finance.api.user.domain.repository.UserRepository;
+import com.board.seochu.finance.api.user.dto.UserPrinciple;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found with -> username or email : " + username)
+                );
+
+        return UserPrinciple.build(user);
+    }
+}
