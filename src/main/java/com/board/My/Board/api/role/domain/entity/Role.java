da34d1c99cf6754c34db297aa2ce1f0fa32daa84
KyoCheol <kok1434@naver.com>
2021-11-18 16:50:00 +0900

    Create Role.java

---
+++ src/main/java/com/board/My/Board/api/role/domain/entity/Role.java
@@ -0,0 +1,32 @@
+package com.board.seochu.finance.api.role.domain.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@ToString
+@EqualsAndHashCode
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+
+    public Role(RoleName name) {
+        this.name = name;
+    }
+
+    public Role() {
+
+    }
+}
