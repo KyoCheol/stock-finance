ccc863880515f469981aac149ed94ca13b58743d
KyoCheol <kok1434@naver.com>
2021-11-26 17:26:18 +0900

    Create Snackbar.vue

---
+++ front/src/components/common/Snackbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-snackbar
+    v-model="snackStore.state.sb"
+    :color="snackStore.state.sb.color"
+    :timeout="3000"
+  >
+    {{ sb.msg }}
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        color="white"
+        text
+        v-bind="attrs"
+        @click="snackStore.state.sb.show = false"
+      >
+        <v-icon dark dense>mdi-close-circle-outline</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["snackStore"]),
+  },
+};
+</script>
