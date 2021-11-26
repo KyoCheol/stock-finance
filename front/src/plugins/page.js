2aa8f0c1fec7d9fb35d6732eb9b464d2cfd0a45a
KyoCheol <kok1434@naver.com>
2021-11-26 17:28:34 +0900

    Create page.js

---
+++ front/src/plugins/page.js
@@ -0,0 +1,28 @@
+const page = {};
+
+page.install = function (Vue) {
+  /*
+   *  페이지 이동
+   * */
+  Vue.prototype.movePage = function (url) {
+    if (this.$router.path !== url) {
+      this.$router.push(url);
+    }
+  };
+
+  /*
+   * 이전 페이지 이동
+   * */
+  Vue.prototype.prevPage = function () {
+    window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
+  };
+
+  /**
+   * 페이지 새로고침
+   */
+  Vue.prototype.refresh = function () {
+    this.$router.go(this.$router.currentRoute);
+  };
+};
+
+export default page;
