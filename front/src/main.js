b6eb9da42aab91305f5866bef0908a345cd9ab22
KyoCheol <kok1434@naver.com>
2021-11-26 17:22:43 +0900

    Update main.js

--- front/src/main.js
+++ front/src/main.js
@@ -2,12 +2,34 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import { store } from "./store";
+import vuetify from "./plugins/vuetify";
 import "vuetify/dist/vuetify.min.css";
+import VuetifyDialogPlugin from "vuetify-dialog";
+import "vuetify-dialog/dist/vuetify-dialog.css";
 
+// 사용자 정의 페이지 관련 함수
+import page from "@/plugins/page";
+// 사용자 정의 dialog 관련 함수
+import dialog from "@/plugins/dialog";
+// 버튼 전역 컴포넌트
+import Button from "@/components/common/Button";
+
+// 사용자 정의 페이지 함수 사용설정
+Vue.use(page);
+// 사용자 정의 dialog 함수 사용설정
+Vue.use(dialog);
 Vue.config.productionTip = false;
+//Vuetify-dialog 사용 설정
+Vue.use(VuetifyDialogPlugin, {
+  context: {
+    vuetify,
+  },
+});
+
+// 버튼 전역 컴포넌트 선언
+Vue.component("Button", Button);
 
 import { getAxios } from "./config/axios/axiosConfig";
-import vuetify from "./plugins/vuetify";
 Vue.prototype.$http = getAxios;
 
 //Vue.use(vuetify, {
