5baff0d5927565e8247692555c09a1f03c98cb79
KyoCheol <kok1434@naver.com>
2021-11-26 17:27:01 +0900

    Create BoardWriteScreen.vue

---
+++ front/src/components/screen/board/BoardWriteScreen.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app id="app">
+    <!-- 추가된 헤더 컴포넌트 사용 -->
+    <Header />
+
+    <Content />
+    <!-- 추가된 풋터 컴포넌트 사용 -->
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import Content from "@/components/board/BoardWrite.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+    Content,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
