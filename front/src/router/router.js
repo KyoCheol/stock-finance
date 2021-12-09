ff9e3cd4e4a993686e0895399d2dfd57b0a96e29
KyoCheol <kok1434@naver.com>
2021-12-09 13:41:12 +0900

    Update router.js

--- front/src/router/router.js
+++ front/src/router/router.js
@@ -1,9 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import loginPage from "@/page/loginPage";
+
 import homeScreen from "@/components/screen/homeScreen";
-import loginScreen from "@/components/screen/loginScreen";
-import registerScreen from "@/components/screen/registerScreen";
+
+import loginScreen from "@/components/screen/login/loginScreen";
+import loginPage from "@/page/loginPage";
+
+import registerScreen from "@/components/screen/login/registerScreen";
+
+import boardListScreen from "@/components/screen/board/BoardListScreen";
+import boardListView from "@/components/board/BoardList";
+
+import boardWriteScreen from "@/components/screen/board/BoardWriteScreen";
+import boardWriteView from "@/components/board/BoardWrite";
+
+import boardDetailScreen from "@/components/screen/board/BoardDetailScreen";
+import boardDetailView from "@/components/board/BoardDetail";
+
+import boardEditScreen from "@/components/screen/board/BoardEditScreen";
+import boardEditView from "@/components/board/BoardEdit";
 
 Vue.use(VueRouter);
 
@@ -21,6 +36,30 @@ const routes = [
   {
     path: "/register",
     component: registerScreen,
+  },
+  {
+    path: "/list",
+    component: boardListScreen,
+    children: [{ path: "/list", name: "boardList", component: boardListView }],
+  },
+  {
+    path: "/write",
+    component: boardWriteScreen,
+    children: [
+      { path: "/write", name: "boardWrite", component: boardWriteView },
+    ],
+  },
+  {
+    path: "/detail",
+    component: boardDetailScreen,
+    children: [
+      { path: "/detail", name: "boardDetail", component: boardDetailView },
+    ],
+  },
+  {
+    path: "/edit",
+    component: boardEditScreen,
+    children: [{ path: "/edit", name: "boardEdit", component: boardEditView }],
   },
 ];
 
