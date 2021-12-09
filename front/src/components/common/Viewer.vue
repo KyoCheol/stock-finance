d6c39080606666ce3ef06b621e6f6869074e4a7f
KyoCheol <kok1434@naver.com>
2021-12-09 13:32:35 +0900

    Create Viewer.vue

---
+++ front/src/components/common/Viewer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Viewer ref="toastViewer" height="500px" />
+  </div>
+</template>
+
+<script>
+import { Viewer } from "@toast-ui/vue-editor";
+import "codemirror/lib/codemirror.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+export default {
+  components: {
+    Viewer,
+  },
+  methods: {
+    setContent(content) {
+      this.$refs.toastViewer.invoke("setMarkdown", content);
+    },
+  },
+};
+</script>
