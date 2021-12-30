882fa1ad148b6c84ee1c43eae3b13b0d3b1ced3e
KyoCheol <kok1434@naver.com>
2021-12-30 13:32:58 +0900

    Update JwtProvider.java

--- src/main/java/com/board/seochu/finance/util/auth/jwt/JwtProvider.java
+++ src/main/java/com/board/seochu/finance/util/auth/jwt/JwtProvider.java
@@ -2,6 +2,8 @@ package com.board.seochu.finance.util.auth.jwt;
 
 import com.board.seochu.finance.api.user.dto.UserPrinciple;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -16,21 +20,55 @@ public class JwtProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
+    public final static long TOKEN_VALIDATION_SECOND = 1000L * 10;
+    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 24 * 2;
+
+    final static public String ACCESS_TOKEN_NAME = "accessToken";
+    final static public String REFRESH_TOKEN_NAME = "refreshToken";
+
     @Value("${hendisantika.app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${hendisantika.app.jwtExpiration}")
-    private int jwtExpiration;
+    private Key getSigningKey(String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
-    public String generateJwtToken(Authentication authentication) {
+    public Claims extractAllClaims(String token) throws ExpiredJwtException {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey(jwtSecret))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUserName(String token) {
+        return extractAllClaims(token).get("username", String.class);
+    }
+
+    public Boolean isTokenExpired(String token) {
+        final Date expiration = extractAllClaims(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(Authentication authentication) {
+        return generateJwtToken(authentication, TOKEN_VALIDATION_SECOND );
+    }
+
+    public String generateRefreshToken(Authentication authentication) {
+        return generateJwtToken(authentication, REFRESH_TOKEN_VALIDATION_SECOND);
+    }
+
+    public String generateJwtToken(Authentication authentication, long expireTime) {
 
         UserPrinciple userPrincipal = (UserPrinciple) authentication.getPrincipal();
 
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .setExpiration(new Date((new Date()).getTime() + expireTime))
+                //.signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(getSigningKey(jwtSecret), SignatureAlgorithm.HS256)
                 .compact();
     }
 
